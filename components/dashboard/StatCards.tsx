70a3e2f9e4cc1c9a5537a9016a55271e564f39f1
utso <utsosarkar7870@gmail.com>
2025-09-18 13:01:15 +0600

    done whatsAppbutton and dashboard design

--- components/dashboard/StatCards.tsx
+++ components/dashboard/StatCards.tsx
@@ -5,13 +5,13 @@ import Link from "next/link";
 import { RxBarChart } from "react-icons/rx";
 
 export default function StatCards({ data }: any) {
-  const { data: totalHost ,loading} = useDataFetch(`/admin/dashboard/stats`);
+  const { data: totalHost, loading } = useDataFetch(`/admin/dashboard/stats`);
   console.log(totalHost?.data?.overview);
-  
+
   const statCards = [
     {
       title: "Total Host",
-      value: totalHost?.data?.overview?.totalHosts || 0,
+      value: totalHost?.data?.overview?.totalSubscriptions || 0,
       link: "/dashboard/subscribers",
       icon: <RxBarChart className="text-greenColor" />,
       timeFrame: "Last Month",
@@ -20,7 +20,7 @@ export default function StatCards({ data }: any) {
       title: "Total Users",
       value: totalHost?.data?.overview?.totalUsers || 0,
       link: "/dashboard/players",
-      icon: <RxBarChart  className="text-teal-400"/>,
+      icon: <RxBarChart className="text-teal-400" />,
       timeFrame: "Last Month",
     },
     {
@@ -30,13 +30,13 @@ export default function StatCards({ data }: any) {
       icon: <RxBarChart className="text-orange-400" />,
       timeFrame: "Last Month",
     },
-   
+
   ];
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-8">
-      
-      {loading ? <Loader/> : statCards?.map((card, idx) => (
+
+      {loading ? <Loader /> : statCards?.map((card, idx) => (
         <Link href={card.link}
           key={idx}
           className="p-4 rounded-lg border border-borderColor flex hover:shadow-[2px_2px_7px_2px_rgba(0,_0,_0,_0.08)] transition-all card flex-col gap-5"
@@ -44,8 +44,8 @@ export default function StatCards({ data }: any) {
           {/* Top Row */}
           <div className="flex items-center justify-between gap-3">
             <div className=" space-y-4">
-            <h4 className="text-base lg:text-lg  font-medium text-descriptionColor dark:text-whiteColor">{card.title}</h4>
-             <div className="text-2xl lg:text-[32px] font-semibold text-black dark:text-whiteColor">{card.value}</div>
+              <h4 className="text-base lg:text-lg  font-medium text-descriptionColor dark:text-whiteColor">{card.title}</h4>
+              <div className="text-2xl lg:text-[32px] font-semibold text-black dark:text-whiteColor">{card.value}</div>
             </div>
             <div>
               <span className="text-3xl">{card.icon}</span>
