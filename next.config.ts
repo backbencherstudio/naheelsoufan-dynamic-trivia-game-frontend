a867d11cce6bb46b6e85b5557c1a4f337d10d7c3
utso sarkar <utsosarkar7870@gmail.com>
2025-12-01 17:10:31 +0600

    update

--- next.config.ts
+++ next.config.ts
@@ -1,56 +1,52 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
-
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "maps.googleapis.com",
+        protocol: 'https',
+        hostname: 'maps.googleapis.com',
       },
       {
-        protocol: "https",
-        hostname: "ceremony-julian-tampa-ken.trycloudflare.com",
+        protocol: 'https',
+        hostname: 'ceremony-julian-tampa-ken.trycloudflare.com',
       },
       {
-        protocol: "https",
-        hostname: "nirob.signalsmind.com",
-      },
-      {
-        protocol: "https",
-        hostname: "firebasestorage.googleapis.com",
-      },
-      {
-        protocol: "https",
-        hostname: "googleapis.com",
-      },
-      {
-        protocol: "http",
-        hostname: "nirob.signalsmind.com",
-      },
-      {
-        protocol: "https",
-        hostname: "signalsmind.com",
-      },
-      {
-        protocol: "https",
-        hostname: "nirob.com",
-      },
-       {
         protocol: 'https',
         hostname: 'nirob.signalsmind.com',
-       
-      },
-       {
-        protocol: 'https',
-        hostname: 'caroll-perihelial-nonnecessitously.ngrok-free.dev',
-       
       },
       {
-        protocol: "http",
-        hostname: "localhost",
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'googleapis.com',
+      },
+      {
+        protocol: 'http',
+        hostname: 'nirob.signalsmind.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'signalsmind.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'naheelsoufan.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'naheelsoufan.signalsmind.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'caroll-perihelial-nonnecessitously.ngrok-free.dev',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
       },
     ],
-
   },
 };
 
